0936977cec659a0cbab99e9ec94521bdc805e52d
MissJacobi <feliciajacobi97.fj@gmail.com>
2026-05-25 14:16:13 +0200

    comment in user

--- src/main/java/se/jensen/felicia/cloudstore/model/User.java
+++ src/main/java/se/jensen/felicia/cloudstore/model/User.java
@@ -14,6 +14,7 @@ import java.util.List;
 @Getter @Setter @NoArgsConstructor
 @ToString
 public class User {
+    //fix getters
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
